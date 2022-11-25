06820f03bb19385d47eff3257cdf0d4084ecad75
António Gonçalves <antonio.goncalves@unparallel.pt>
2022-11-25 10:39:52 +0000

    Final version of Use Case card

--- blocks/iotcat-usecase-card.php
+++ blocks/iotcat-usecase-card.php
@@ -2,13 +2,13 @@
 
 
 
-function iotcat_usecase_card_render_box($label, $value, $last_square = false){
+function iotcat_usecase_card_render_box($label, $value, $color,$last_square = false){
 
     $border_class = $last_square?'':'border-end';
-
+    $color_style = $color?"color: ".$color."!important":"";
     return '
     <div class="bg-light flex-grow-1 text-center pe-3 ps-3  border-white border-top '.$border_class.'">
-        <h6 class="text-primary small mt-2 mb-0 ">
+        <h6 class="text-primary small mt-2 mb-0 " style="'.$color_style.'">
             '.$value.'
         </h6>
         <h6 class="small mb-2 mt-0">
@@ -18,13 +18,13 @@ function iotcat_usecase_card_render_box($label, $value, $last_square = false){
     ';
 }
 
-function iotcat_usecase_card_render_boxes($boxes){
+function iotcat_usecase_card_render_boxes($boxes,$color){
     $html ='';
 
     if(is_array($boxes)){
         foreach($boxes as $key=>$box){
             $last_square = $key === count($boxes) -1;
-            $html = $html.iotcat_usecase_card_render_box($box["label"],$box["value"],$last_square);
+            $html = $html.iotcat_usecase_card_render_box($box["label"],$box["value"],$color,$last_square);
         }
     }
     return $html;
@@ -46,17 +46,27 @@ function iotcat_usecase_card_render_callback( $block_attributes, $content, $bloc
     }*/
     iotcat_log_me($block_attributes);
     $boxes = $block_attributes["boxes"];
-    return '
-
+    $style = "";
+    $color = null;
+    if(array_key_exists("color", $block_attributes)){
+     $color = $block_attributes["color"];
+      $style ='{"borderColor":"'.$color.'"}';
+    }
+    return do_blocks( 
+        '
+        <!-- wp:unparallel/base-card '.$style.' -->
             <div >
                 <h5 class="mt-0 pt-3 mb-3 fw-bold text-center text-truncate border-top border-white">
                     '.$block_attributes["title"].'
                 </h5>
                 <div class="d-flex flex-wrap ">
-                    '.iotcat_usecase_card_render_boxes($boxes).'
+                    '.iotcat_usecase_card_render_boxes($boxes,$color).'
             </div>
+        <!-- /wp:unparallel/base-card -->
+    '
 
-    ';
+    ) ;
+
 
 }
   function iotcat_register_usecase_card_block() {
