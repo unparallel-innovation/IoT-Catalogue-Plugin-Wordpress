547bce2e49544557b24f69559c55d1afec44c167
António Gonçalves <antonio.goncalves@unparallel.pt>
2023-12-12 13:51:52 +0000

    Updated version number

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       IoT Catalogue Integration
  * Description:       Display in WordPress content from IoT Catalogue
- * Version:           1.9.0
+ * Version:           1.9.1
  * Author:            UNPARALLEL Innovation, Lda
  * Author URI:        https://www.unparallel.pt
  */
